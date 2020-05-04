28f6b0326952a3125773909759e9a2419690cb4a
Adam Gardner <adam.gardner4991@gmail.com>
2020-05-04 14:22:28 +0100

    tricksy challenge 2 (sum 117 multioples of 3 and 7) completed

---
+++ tricksy2.php
@@ -0,0 +1,17 @@
+<?php 
+
+$total = 0;
+$totalNums = 0;
+$index = 0;
+
+while ($totalNums < 117) {
+    $index += 1;
+
+    if ($index % 3 === 0 || $index % 7 === 0) {
+        $total += $index;
+        $totalNums += 1;
+    }
+    
+}
+
+var_dump($total);
